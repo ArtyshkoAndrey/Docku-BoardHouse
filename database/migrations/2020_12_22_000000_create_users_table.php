0b27efcc97961f6cc7011507a9d52a290953c8dc
Fulliton <artyshko.andrey@gmail.com>
2020-12-19 00:24:27 +0700

    Edit: New user columns

---
+++ database/migrations/2020_12_22_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('users', function (Blueprint $table) {
+      $table->id();
+      $table->string('name');
+      $table->text('avatar')->nullable();
+      $table->string('email')->unique();
+      $table->timestamp('email_verified_at')->nullable();
+      $table->string('password');
+      $table->text('address')->nullable();
+      $table->string('post_code')->nullable();
+      $table->foreignId('country_id')->nullable()->constrained();
+      $table->foreignId('city_id')->nullable()->constrained();
+      $table->rememberToken();
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('users');
+  }
+}
