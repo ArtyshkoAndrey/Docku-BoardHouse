231bf8439a582dfce24a855996835df3a98b3fc7
Fulliton <artyshko.andrey@gmail.com>
2020-12-19 00:37:10 +0700

    Edit: Ide Helper

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\City
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $country_id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Country|null $country
+ * @method static Builder|City newModelQuery()
+ * @method static Builder|City newQuery()
+ * @method static Builder|City query()
+ * @method static Builder|City whereCountryId($value)
+ * @method static Builder|City whereCreatedAt($value)
+ * @method static Builder|City whereId($value)
+ * @method static Builder|City whereName($value)
+ * @method static Builder|City whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class City extends Model
 {
   use HasFactory;
--- app/Models/User.php
+++ app/Models/User.php
@@ -18,31 +18,43 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Carbon;
 
 /**
-* App\Models\User
-*
-* @property int $id
-* @property string $name
-* @property string $email
-* @property Carbon|null $email_verified_at
-* @property string $password
-* @property string|null $remember_token
-* @property Carbon|null $created_at
-* @property Carbon|null $updated_at
-* @property-read DatabaseNotificationCollection|DatabaseNotification[] $notifications
-* @property-read int|null $notifications_count
-* @method static Builder|User newModelQuery()
-* @method static Builder|User newQuery()
-* @method static Builder|User query()
-* @method static Builder|User whereCreatedAt($value)
-* @method static Builder|User whereEmail($value)
-* @method static Builder|User whereEmailVerifiedAt($value)
-* @method static Builder|User whereId($value)
-* @method static Builder|User whereName($value)
-* @method static Builder|User wherePassword($value)
-* @method static Builder|User whereRememberToken($value)
-* @method static Builder|User whereUpdatedAt($value)
-* @mixin Eloquent
-*/
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read DatabaseNotificationCollection|DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static Builder|User newModelQuery()
+ * @method static Builder|User newQuery()
+ * @method static Builder|User query()
+ * @method static Builder|User whereCreatedAt($value)
+ * @method static Builder|User whereEmail($value)
+ * @method static Builder|User whereEmailVerifiedAt($value)
+ * @method static Builder|User whereId($value)
+ * @method static Builder|User whereName($value)
+ * @method static Builder|User wherePassword($value)
+ * @method static Builder|User whereRememberToken($value)
+ * @method static Builder|User whereUpdatedAt($value)
+ * @mixin Eloquent
+ * @property string|null $avatar
+ * @property string|null $address
+ * @property string|null $post_code
+ * @property int|null $country_id
+ * @property int|null $city_id
+ * @property-read City|null $city
+ * @property-read Country|null $country
+ * @method static Builder|User whereAddress($value)
+ * @method static Builder|User whereAvatar($value)
+ * @method static Builder|User whereCityId($value)
+ * @method static Builder|User whereCountryId($value)
+ * @method static Builder|User wherePostCode($value)
+ */
 class User extends Authenticatable
 {
   use HasFactory, Notifiable;
