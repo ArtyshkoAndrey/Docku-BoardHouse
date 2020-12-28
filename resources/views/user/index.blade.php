1185c9fa14dda1bdfb327cbbb7f6fde67d71f722
Hugant Mirron <Hugant@users.noreply.github.com>
2020-12-29 00:17:23 +0700

    Fixed mountains, added catalog item title

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -14,13 +14,10 @@
   </section>
 
   <section>
-    <div class="container">
-      <div class="row">
-        <div class="col-12 col-md-6 col-lg-4 col-xl-3 p-0" v-for="i in 4">
-          @include('user.layouts.item')
-        </div>
-      </div>
-    </div>
+    @include('user.layouts.item-catalog')
+    @include('user.layouts.item-catalog')
+    @include('user.layouts.item-catalog')
+    @include('user.layouts.item-catalog')
   </section>
 @endsection
 
