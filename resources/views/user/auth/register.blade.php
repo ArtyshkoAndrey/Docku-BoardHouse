05c4b704b854195492c602a019fc7c98b0123dfa
Hugant Mirron <Hugant@users.noreply.github.com>
2021-01-03 21:54:29 +0700

    Register page

--- resources/views/user/auth/register.blade.php
+++ resources/views/user/auth/register.blade.php
@@ -1,77 +1,138 @@
 @extends('user.layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+  <div class="container-fluid d-flex align-items-center">
+    <div class="row w-md-100 d-flex justify-content-center">
+      <div class="col-lg-5 col-md-6 col-12">
+        <div class="row justify-content-center">
+          <div class="col-md-5 col-6">
+            <img src="{{ asset('images/logo-dark.svg') }}" alt="logo" class="img-fluid mb-5 mx-auto d-block">
+          </div>
         </div>
+        <div class="card rounded-0">
+          <div class="row m-0 flex-nowrap text-center">
+            <div class="col bg-gray px-4 px-md-5 py-4 link-inverse-login-register font-weight-bolder">
+              <a href="{{ route('login') }}" class="text-decoration-none inverse d-flex justify-content-center align-items-center"><i class="icon icon-user icon-1_5x"></i>Войти</a>
+            </div>
+            <div class="col px-5 py-4 font-weight-bolder d-flex justify-content-center align-items-center"><i class="icon icon-register icon-1_5x color-orange "></i>Регистрация</div>
+          </div>
+          <div class="card-body p-4">
+            <div class="row">
+              <div class="col-12">
+                <h5 class="text-center w-100">Регистрация черз соц сеть</h5>
+              </div>
+              <div class="col-12">
+                <div class="row p-0 m-0">
+                  <div class="col-4 p-0 pr-1">
+                    <a href="#" class="btn d-block btn-outline-social rounded-0 d-flex justify-content-center h-100" id="google"><i class="icon icon-google"></i> <span class="d-none d-md-block">Google</span></a>
+                  </div>
+                  <div class="col-4 p-0 px-1">
+                    <a href="#" class="btn d-block btn-outline-social rounded-0 d-flex justify-content-center h-100" id="fb"><i class="icon icon-facebook"></i> <span class="d-none d-md-block">Facebook</span></a>
+                  </div>
+                  <div class="col-4 p-0 pl-1">
+                    <a href="#" class="btn d-block btn-outline-social rounded-0 d-flex justify-content-center align-items-center h-100" id="vk"><i class="fab fa-vk mr-md-1"></i> <span class="d-none d-md-block">VKontakte</span></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 mt-3">
+                <h5 class="text-center">Или укажите логин и пароль</h5>
+              </div>
+              <div class="col-12 mt-3">
+                <form action="{{ route('register') }}" method="post">
+                  @csrf
+                  <div class="form-outline mb-4">
+                    <input type="text" id="first_name" name="first_name" class="form-control @error('first_name') is-invalid @enderror" value="{{ old('first_name') }}" required/>
+                    <label class="form-label" for="first_name">Имя</label>
+                  </div>
+                  @error('first_name')
+                  <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                  </span>
+                  @enderror
+
+                  <div class="form-outline mb-4 ">
+                    <input type="text" id="last_name" name="last_name" class="form-control @error('last_name') is-invalid @enderror" value="{{ old('last_name') }}" required/>
+                    <label class="form-label" for="last_name">Фамилия</label>
+                  </div>
+                  @error('last_name')
+                  <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                  </span>
+                  @enderror
+
+                  <div class="form-outline mb-4">
+                    <input type="email" id="email" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" required/>
+                    <label class="form-label" for="email">Email</label>
+                  </div>
+                  @error('email')
+                  <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                  </span>
+                  @enderror
+
+                  <div class="form-outline mb-4">
+                    <input type="password" id="password" name="password" class="form-control @error('password') is-invalid @enderror" required/>
+                    <label class="form-label" for="password">Пароль</label>
+                  </div>
+                  @error('password')
+                  <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                  </span>
+                  @enderror
+
+                  <div class="form-outline mb-4">
+                    <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" required/>
+                    <label class="form-label" for="password_confirmation">Повторите пароль</label>
+                  </div>
+
+                  <button id="submitter" class="btn btn-dark w-100 d-block mt-3" style="height: 43px;" disabled>Зарегистрироваться</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
+@endsection
+
+@section('js')
+  <script>
+    let checker = {
+      first_name: false,
+      last_name: false,
+      password: false,
+      password_confirmation: false,
+      email: false
+    }
+    $( "input" ).focus(function() {
+      $(this).parent().addClass('focus')
+    });
+    $('input').focusout(function() {
+      $(this).parent().removeClass('focus')
+    });
+    for (let key in checker) {
+      $('#'+key).on('keydown keyup change', function () {
+        let charLength = $(this).val().length;
+        if (charLength >= 3) {
+          checker[key] = true
+          console.log(disabled(checker))
+          if(disabled(checker)) {
+            $('#submitter').attr('disabled', false)
+          } else {
+            $('#submitter').attr('disabled', true)
+          }
+        }
+      })
+    }
+    function disabled(checker) {
+      let v = true
+      for (let key in checker) {
+        if (!checker[key]) {
+          v = false
+        }
+      }
+      return v
+    }
+  </script>
 @endsection
