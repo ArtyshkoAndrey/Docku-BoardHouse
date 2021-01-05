be5f027bafa0aa3db0a760e61da6d39e7e63175a
Fulliton <artyshko.andrey@gmail.com>
2021-01-05 13:33:18 +0700

    Edit: PHPDoc

--- app/Models/ProductSkus.php
+++ app/Models/ProductSkus.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Support\Carbon;
 
 /**
  * App\Models\ProductSkus
@@ -13,19 +15,19 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
  * @property int $stock
  * @property int $product_id
  * @property int $skus_id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \App\Models\Product|null $product
- * @property-read \App\Models\Skus|null $skus
- * @method static \Illuminate\Database\Eloquent\Builder|ProductSkus newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|ProductSkus newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|ProductSkus query()
- * @method static \Illuminate\Database\Eloquent\Builder|ProductSkus whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|ProductSkus whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|ProductSkus whereProductId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|ProductSkus whereSkusId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|ProductSkus whereStock($value)
- * @method static \Illuminate\Database\Eloquent\Builder|ProductSkus whereUpdatedAt($value)
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Product|null $product
+ * @property-read Skus|null $skus
+ * @method static Builder|ProductSkus newModelQuery()
+ * @method static Builder|ProductSkus newQuery()
+ * @method static Builder|ProductSkus query()
+ * @method static Builder|ProductSkus whereCreatedAt($value)
+ * @method static Builder|ProductSkus whereId($value)
+ * @method static Builder|ProductSkus whereProductId($value)
+ * @method static Builder|ProductSkus whereSkusId($value)
+ * @method static Builder|ProductSkus whereStock($value)
+ * @method static Builder|ProductSkus whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class ProductSkus extends Model
