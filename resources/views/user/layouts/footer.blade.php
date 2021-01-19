eb3b17b2b2835fe915eb5c80114a4d68a5f5c595
Fulliton <artyshko.andrey@gmail.com>
2021-01-19 16:29:34 +0700

    Разработчики в футере

--- resources/views/user/layouts/footer.blade.php
+++ resources/views/user/layouts/footer.blade.php
@@ -4,7 +4,7 @@
       <div class="col-md-6 col-lg-4 justify-content-between">
         <div class="d-flex flex-column align-items-center">
           <span>Есть вопросы? Свяжитесь с нами :)</span>
-          <a href="tel:" class="number">+7 (747) 556-23-83</a>
+          <a href="tel:+77475562383" class="number">+7 (747) 556-23-83</a>
         </div>
         <span class="text-center">© dockuboardhouse.com 2020 – магазин сноубордов</span>
       </div>
@@ -24,6 +24,12 @@
         <span class="title">Как нас найти</span>
         <span>РК, Алматы Мкр. Самал-3,1 050059</span>
         <span>Аль Фараби угол ул. Достык, слева от центрального входа в ТРЦ “Ритц Палас” </span>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 text-center d-inline-block">
+        <span>Powered by <a href="https://www.vk.com/Fulliton" style="color: #1266f1;" target="_blank">Fulliton</a></span>
+        <span class="ml-3">Powered by <a href="https://www.vk.com/Fulliton" style="color: #1266f1;" target="_blank">Fulliton</a></span>
       </div>
     </div>
   </div>
